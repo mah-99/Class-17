0b54322575120be3f0095fab9c98714e471a97ce
mah-99 <mah.wd.bd@gmail.com>
2023-08-24 12:56:13 +0600

    a

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+document.getElementById('btn').addEventListener('click', function(){
+    document.getElementById('sidebar').style.marginLeft = 0;
+})
+
+document.getElementById('btn').addEventListener('dblclick', function(){
+    document.getElementById('sidebar').style.marginLeft = '-300px';
+})
